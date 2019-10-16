4abbd209ff923e0d4c53658c1aecb6cecc10a4f4
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-10-16 15:15:32 +0300

    Updates

---
+++ precache-manifest.92dba9b86f553fde3c2cfe84676963ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0e4c643591bc2f2cb3c746793e82b5",
+    "url": "/api-pixabay/index.html"
+  },
+  {
+    "revision": "d9d7e19d2fe1feac7d10",
+    "url": "/api-pixabay/static/css/main.f4023a46.chunk.css"
+  },
+  {
+    "revision": "d604d1c26fd6b472e295",
+    "url": "/api-pixabay/static/js/2.856f08d8.chunk.js"
+  },
+  {
+    "revision": "d9d7e19d2fe1feac7d10",
+    "url": "/api-pixabay/static/js/main.87571b24.chunk.js"
+  },
+  {
+    "revision": "03002fb4e7d94ac7ecf7",
+    "url": "/api-pixabay/static/js/runtime-main.a526381e.js"
+  }
+]);
